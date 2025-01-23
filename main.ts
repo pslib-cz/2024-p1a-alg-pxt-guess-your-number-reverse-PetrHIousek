c93d0ec373fa90a81a48559570aaf08eef3ab488
Hlusinka <hlusinp@gmail.com>
2025-01-23 22:59:03 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-let lowerBound = 1
-let upperBound = 99
+let MinimalNumber = 1
+let MaximalNumber = 99
 let guess = 0
 let attempts = 0
 
 basic.showString("Mysli na cislo")
 
 while (true) {
-    guess = Math.floor((lowerBound + upperBound) / 2)
+    guess = Math.floor((MinimalNumber + MaximalNumber) / 2)
     attempts += 1
     whaleysans.showNumber(guess)
     basic.pause(500)
@@ -14,12 +14,12 @@ while (true) {
     while (true) {
         if (input.buttonIsPressed(Button.A)) {
             // Číslo je menší
-            upperBound = guess - 1
+            MaximalNumber = guess - 1
             break
         }
         if (input.buttonIsPressed(Button.B)) {
             // Číslo je větší
-            lowerBound = guess + 1
+            MinimalNumber = guess + 1
             break
         }
         if (input.logoIsPressed()) {
